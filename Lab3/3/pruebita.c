99a1b60230b52c9f05c437f80feff8f6e52aa604
FrMiMoAl <32112849+FrMiMoAl@users.noreply.github.com>
2025-08-27 19:22:38 -0400

    Update pruebita.c
    si

--- Lab3/3/pruebita.c
+++ Lab3/3/pruebita.c
@@ -1,119 +1,74 @@
 //*****************************************************************************
-//
-// blinky.c - Simple example to blink the on-board LED.
-//
-// Copyright (c) 2013-2017 Texas Instruments Incorporated.  All rights reserved.
-// Software License Agreement
-// 
-// Texas Instruments (TI) is supplying this software for use solely and
-// exclusively on TI's microcontroller products. The software is owned by
-// TI and/or its suppliers, and is protected under applicable copyright
-// laws. You may not combine this software with "viral" open-source
-// software in order to form a larger program.
-// 
-// THIS SOFTWARE IS PROVIDED "AS IS" AND WITH ALL FAULTS.
-// NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT
-// NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. TI SHALL NOT, UNDER ANY
-// CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR CONSEQUENTIAL
-// DAMAGES, FOR ANY REASON WHATSOEVER.
-// 
-// This is part of revision 2.1.4.178 of the EK-TM4C1294XL Firmware Package.
-//
+// blinky_counter.c - LEDs PN1/PN0/PF4/PF0 muestran un contador 0..15.
+// Botones: PJ0 (++), PJ1 (--). EK-TM4C1294XL (TM4C1294NCPDT).
 //*****************************************************************************
 
 #include <stdint.h>
 #include <stdbool.h>
 
 #include "inc/hw_memmap.h"
-#include "driverlib/debug.h"
-#include "driverlib/gpio.h"
 #include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "driverlib/debug.h"
 
-
-//*****************************************************************************
-//
-//! \addtogroup example_list
-//! <h1>Blinky (blinky)</h1>
-//!
-//! A very simple example that blinks the on-board LED using direct register
-//! access.
-//
-//*****************************************************************************
-
-//*****************************************************************************
-//
-// The error routine that is called if the driver library encounters an error.
-//
-//*****************************************************************************
+// Si compilas con DEBUG, TivaWare pide este manejador:
 #ifdef DEBUG
-void
-__error__(char *pcFilename, uint32_t ui32Line)
-{
+void __error__(char *pcFile, uint32_t ui32Line) {
     while(1);
 }
 #endif
 
-//*****************************************************************************
-//
-// Blink the on-board LED.
-//
-//*****************************************************************************
+// Mapeo de LEDs de la placa: D1=PN1, D2=PN0, D3=PF4, D4=PF0
+static const uint32_t selector[4] = { GPIO_PORTN_BASE, GPIO_PORTN_BASE, GPIO_PORTF_BASE, GPIO_PORTF_BASE };
+static const uint8_t  pins[4]  = { GPIO_PIN_1,      GPIO_PIN_0,      GPIO_PIN_4,      GPIO_PIN_0      };
 
-static inline void leds_off(void) {
-    GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_0);
-    GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_1);
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_0);
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_4);
-    
-    GPIOPinTypeGPIOInput(GPIO_PORTJ_BASE, GPIO_PIN_0 | GPIO_PIN_1);
-    GPIOPadConfigSet(GPIO_PORTJ_BASE,GPIO_PIN_0 | GPIO_PIN_1,GPIO_STRENGTH_2MA,GPIO_PIN_TYPE_STD_WPU);
-    
-    const uint8_t pins[] = { GPIO_PIN_1, GPIO_PIN_0, GPIO_PIN_4, GPIO_PIN_0};
-    const uint8_t selector[] =(GPIO_PORTN_BASE,GPIO_PORTN_BASE,GPIO_PORTF_BASE,GPIO_PORTF_BASE);    
-  
-}
-
-int 
-main(void)
+int main(void)
 {
-    bool push1 = (GPIOPinRead(GPIO_PORTJ_BASE, GPIO_PIN_0) & GPIO_PIN_0) == 0;
-    bool push2 = (GPIOPinRead(GPIO_PORTJ_BASE, GPIO_PIN_1) & GPIO_PIN_1) == 0;
-
     uint32_t sysclk = SysCtlClockFreqSet(
         SYSCTL_OSC_MAIN | SYSCTL_USE_PLL | SYSCTL_XTAL_25MHZ | SYSCTL_CFG_VCO_320,
         120000000UL
     );
-    
+
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPION))
-    {}
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOJ);
+
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPION)) {}
     while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF)) {}
-    
-    
-   int cont=0;
-    while(1)
-    {
-	bool sw1 = (GPIOPinRead(GPIO_PORTJ_BASE, GPIO_PIN_0) & GPIO_PIN_0) == 0;
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOJ)) {}
+
+    for (int i = 0; i < 4; i++) {
+        GPIOPinTypeGPIOOutput(selector[i], pins[i]);
+        GPIOPinWrite(selector[i], pins[i], 0); 
+    }
+    GPIOPinTypeGPIOInput(GPIO_PORTJ_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+    GPIOPadConfigSet(GPIO_PORTJ_BASE, GPIO_PIN_0 | GPIO_PIN_1,
+                     GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
+
+    uint32_t counter = 0;
+
+    while (1) {
+        bool sw1 = (GPIOPinRead(GPIO_PORTJ_BASE, GPIO_PIN_0) & GPIO_PIN_0) == 0;
         bool sw2 = (GPIOPinRead(GPIO_PORTJ_BASE, GPIO_PIN_1) & GPIO_PIN_1) == 0;
 
         if (sw1 && counter < 15) {
             counter++;
-            SysCtlDelay(sysclk/30);  
+            SysCtlDelay(sysclk / 30); 
         }
         if (sw2 && counter > 0) {
             counter--;
-            SysCtlDelay(sysclk/30);  
+            SysCtlDelay(sysclk / 30);
         }
 
-        for (int j=0; j<4; j++) {
-		uint8_t bit = (counter >> j) & 1;
-		if (bit == 1) {
-		    GPIOPinWrite(ports[j], pins[j], pins[j]);  // LED ON
-		} else {
-		    GPIOPinWrite(ports[j], pins[j], 0);        // LED OFF
-		}
+        for (int j = 0; j < 4; j++) {
+			
+            uint8_t bit = (counter >> j) & 1;
+            if (bit) {
+                GPIOPinWrite(selector[j], pins[j], pins[j]);
+            } else {
+                GPIOPinWrite(selector[j], pins[j], 0);
+            }
+        }
 
-}
+    }
 }
