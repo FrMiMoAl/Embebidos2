63d9cd35bc6702af57c6edf061fd894d6dd00d16
Globetrotter163 <137434087+Globetrotter163@users.noreply.github.com>
2025-10-24 00:29:39 -0400

    Pls use git

--- CyberTruckV2/PiPicoW/main.py
+++ CyberTruckV2/PiPicoW/main.py
@@ -1,1 +1,16 @@
 import machine
+
+class MotorController:
+    def __init__(self, A1_IN1, A1_IN2, A2_IN1, A2_IN2, B1_IN1, B1_IN2, B2_IN1, B2_IN2):
+        self.A1_IN1 = machine.Pin(A1_IN1, machine.Pin.OUT)
+        self.A1_IN2 = machine.Pin(A1_IN2, machine.Pin.OUT)
+        self.A2_IN1 = machine.Pin(A2_IN1, machine.Pin.OUT)
+        self.A2_IN2 = machine.Pin(A2_IN2, machine.Pin.OUT)
+        self.B1_IN1 = machine.Pin(B1_IN1, machine.Pin.OUT)
+        self.B1_IN2 = machine.Pin(B1_IN2, machine.Pin.OUT)
+        self.B2_IN1 = machine.Pin(B2_IN1, machine.Pin.OUT)
+        self.B2_IN2 = 
+
+    def set_speed(self, speed):
+        # Placeholder for setting motor speed
+        pass
