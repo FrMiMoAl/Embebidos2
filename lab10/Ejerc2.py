1e57e10a189bd5948e67f5a2adba93eafe9793fa
Samuel Guzman <dsamuelguzman@gmail.com>
2025-11-18 19:22:08 -0400

    cambios 10 y 11 labos

--- lab10/Ejerc2.py
+++ lab10/Ejerc2.py
@@ -1,4 +1,3 @@
-
 import tensorflow as tf
 import numpy as np
 
@@ -6,13 +5,12 @@ X_train = np.array([-2, -1, 0, 1, 2, 3], dtype=float)
 Y_train = 3 * X_train + 2
 
 model = tf.keras.Sequential([
-    tf.keras.layers.Dense(8, activation='relu', input_shape=[1]),
-    tf.keras.layers.Dense(4, activation='relu'),
-    tf.keras.layers.Dense(1)
+    tf.keras.layers.Dense(1, input_shape=[1])
 ])
-model.compile(optimizer='adam', loss='mse')
-model.fit(X_train, Y_train, epochs=500, verbose=0)
 
+model.compile(optimizer='sgd', loss='mse')  
+
+model.fit(X_train, Y_train, epochs=500, verbose=0)
 
 print("X=5  → ", model.predict(np.array([5.0]))[0][0])
 print("X=3.3→ ", model.predict(np.array([3.3]))[0][0])
